5cc4b5537f3672f26b5d9e9b7490aab7f70bcd12
unknown <70019070+G20-00@users.noreply.github.com>
2021-05-21 20:26:25 -0500

    prueba

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -5,7 +5,7 @@ public class Customer  extends Debtors {
 	
 	public Customer(String name, String documentType, String id, String address, String phone) {
 		super(name, documentType, id, address, phone);
-		
+		//System.out.println();
 		
 	}
 
